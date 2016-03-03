65dc074b6dea02f422fe39f42e9c7d0d3b057f57
wil <devnull64@vfemail>
2016-03-03 13:09:44 -0500

    Get the font size right

--- torrent-browser.py
+++ torrent-browser.py
@@ -1,9 +1,28 @@
-import gtk, webkit, urllib
+import gtk, webkit, urllib, re
 
+head = '''
+<html>
+<head>
+<style>
+p {
+    font-size: 60%;
+}
+</style>
+<title>Page title here</title>
+</head>
+<body>
+    <div>
+        <p>'''
+foot = '</p></div></body></html>'
 def omdb_call(name,year):
     url = 'http://www.omdbapi.com/?t={0}&y={1}&plot=full&r=json'.format(name,year)
-    return urllib.urlopen(url).read()
-test_stuff = omdb_call('brick','2006')
+    data = urllib.urlopen(url).read()
+    data = data.replace(',', '<br>')
+    data = data.replace('"','')
+    data = data.replace('{','')
+    data = data.replace('}','')
+    return data
+test_stuff = omdb_call('brick','2005')
 
 # Create window
 w = gtk.Window()
@@ -12,7 +31,7 @@ w.connect("destroy", gtk.main_quit)
 
 window = webkit.WebView()
 # Load an HTML
-window.load_html_string(str(test_stuff), "file:///")
+window.load_html_string(head + test_stuff + foot, "file:///")
 
 w.add(window)
 w.show_all()
